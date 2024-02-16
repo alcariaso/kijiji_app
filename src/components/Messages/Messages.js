b865c370ee0f954db99784d45d786a0a4084794c
Aldee <aldee.cariaso@gmail.com>
2024-02-16 05:55:57 -0700

    message.js initial html and styling

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,21 +1,41 @@
+import react from 'react';
 import '../Messages/Messages.scss';
+import sentIcon from "../../assets/images/icons8-send-48.png"
 
 export const Messages = ()  => {
 
     return (
-        <main>
+        <main className='messages'>
             {/* space for header */}
-            <div >
-                <div>
+            <div className='messageBox'>
+
+                <div className='messageBox__main'>
+                    <div className='message--sent'>
+                        <p className='message__text'>Sample Text</p>
+                        <p className='message__notif'>Read</p>
+                    </div>
+
+                    <div className='message--recieved'>
+                        <p className='message__text'>Sample Text</p>
+                        <p className='message__notif'>Read</p>
+
+                    </div>
+
+                    <div className='message--sent'>
+                        <p className='message__text'>Sample Text</p>
+                        <p className='message__notif'>Read</p>
+
+                    </div>
 
                 </div>
 
-                <div>
-
+                <div className='messageBox__AddOn'>
+                    <button className='button' type='submit'></button>
                 </div>
 
-                <div>
-
+                <div className='messageBox__textBox'>
+                    <input className='input'></input>
+                    <img className='img' src={sentIcon}></img>
                 </div>
 
             </div>
