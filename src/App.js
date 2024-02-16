f53d85137cb5cf998f40833a5563915e71835592
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-02-16 13:16:10 -0800

    google auth

--- src/App.js
+++ src/App.js
@@ -2,39 +2,27 @@ import Messages from './components/Messages/Messages';
 import Header from './components/Header/Header';
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import Login from './components/Login/Login'
-import Logout from './components/Login/Logout'
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
+// import Logout from './components/Login/Logout'
+
+// import { gapi } from 'gapi-script';
 
 
 
-const clientId = "391910508656-rle8fb2gr1b6k3fu19f60faudgoi3voa.apps.googleusercontent.com";
+// const clientId = "391910508656-rle8fb2gr1b6k3fu19f60faudgoi3voa.apps.googleusercontent.com";
 
 function App() {
 
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
 
-    gapi.load('client:auth2', start);
-  });
   
   return (
-    <BrowserRouter>
-      <Header/>
+    <>
+ 
   <Login/>
-  <Logout/>
-      <Routes>
-        <Route path='/' element={<Messages/>}></Route>
-      </Routes>
-    </BrowserRouter>
-    
-  );
+  </>
+ )
   }
 
 export default App;
