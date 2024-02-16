01ff138af2f2ad5db6a7237c6a66873fa8dc3efb
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-02-16 08:58:38 -0800

    google

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
 import './App.css';
+import Login from './components/Login/Login'
+import Logout from './components/Login/Logout'
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const clientId = "391910508656-rle8fb2gr1b6k3fu19f60faudgoi3voa.apps.googleusercontent.com";
 
 function App() {
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+
+    gapi.load('client:auth2', start);
+  });
   return (
     <div className="App">
   <>
+  <Login/>
+  <Logout/>
   </>
     </div>
   );
