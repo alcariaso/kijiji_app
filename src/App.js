b27220e01ef287de59bcfd254b1b267218d7a0b2
Aldee <aldee.cariaso@gmail.com>
2024-02-15 23:49:58 -0700

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import Messages from './components/Messages/Messages';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  <>
-  </>
+
+    <Messages/>
+    
     </div>
   );
 }
---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,28 @@
+import '../Messages/Messages.scss';
+
+export const Messages = ()  => {
+
+    return (
+        <main>
+            {/* space for header */}
+            <div >
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+            </div>
+            
+
+        </main>
+    )
+}
+
+export default Messages
