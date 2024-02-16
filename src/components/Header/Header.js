e2cbcc11da597faf23c3b45ce46223acd17821d5
Aldee <aldee.cariaso@gmail.com>
2024-02-16 08:16:26 -0700

    styling changes in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import '../Header/Header.scss';
 import logo from '../../assets/images/logo.png';
 import heart from '../../assets/images/heart.png';
 import notification from '../../assets/images/notification.png';
+import location from '../../assets/images/location.png';
 import chat from '../../assets/images/chat.png';
 
 
@@ -15,18 +16,19 @@ const Header = () => {
             <nav className="nav">
                 <img className="logo" src={logo}></img>
                 <div className="nav__search">
-                    <input placeholder="What are you looking for?"></input>
-                    <button></button>
-                    <img src=''></img>
+                    <input className="input " placeholder="What are you looking for?"></input>
+                    <button className="button">All Categories</button>
+                    <img className="map" src={location}></img>
+                    <div className="location">Ontario</div>
 
                 </div>
 
                 <ul className="nav__list">
-                    <li className="items"><img src={heart}></img></li>
-                    <li className="items"><img src={notification}></img></li>
-                    <li className="items" ><img src={chat}></img></li>
-                    <li className="items">A</li>
-                    <li className="items"> Post Ad</li>
+                    <li className="items"><img className="img" src={heart}></img></li>
+                    <li className="items"><img  className="img" src={notification}></img></li>
+                    <li className="items" ><img className="img" src={chat}></img></li>
+                    <li className="items"><div className="profile">A</div></li>
+                    <li className="items"><div className="ad">Post Ad</div></li>
                 </ul>
             </nav>
         </header>
